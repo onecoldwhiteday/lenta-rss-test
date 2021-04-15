0ac94c1b5e522b6e9a879a45189c0fcc1b0d6f2c
Nikita Rychagov <35203072+onecoldwhiteday@users.noreply.github.com>
2021-04-16 02:35:49 +0300

    fix: remove redundant import

--- src/app/news/news-routing.module.ts
+++ src/app/news/news-routing.module.ts
@@ -1,6 +1,5 @@
 import { RouterModule, Routes } from "@angular/router";
 import { NewsTableComponent } from "./news-table/news-table.component";
-import { NewsResolver } from "./news.resolver";
 
 const routes: Routes = [
   {
