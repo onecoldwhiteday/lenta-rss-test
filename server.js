86b2e4c655542c7e1a158332289b8ff84c83932b
Nikita Rychagov <35203072+onecoldwhiteday@users.noreply.github.com>
2021-04-17 15:57:09 +0300

    test

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/*'));
+app.use(express.static(__dirname + '/dist/*')); // FIXME: find the proper path
 
 app.get('/*', (req, res, next) => {
-  res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/dictionary/index.html')); // FIXME: find the proper path
 });
 
 
